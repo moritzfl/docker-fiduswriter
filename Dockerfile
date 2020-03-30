20c4ab133f8b3ae05e0684bb1045efae5619f595
yala <yala@allmende.io>
2020-03-31 01:17:04 +0200

    update Dockerfile and start script
    - Dockerfile
      - increase version to latest
      - introduce convention to consider UID:GID 1000:1000
      - add dependencies for PostgreSQL and 12 factor deployment
      - install Fidus Writer with plugins
    
    - start-fiduswriter.sh
      - enforce UID:GID on mounted volumes
      - reorder configuration and media actions before setup takes place,
        in so the correct configuration is applied

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,18 @@
 # vim: set ts=4 sw=4 sts=0 sta et :
 FROM ubuntu:18.04
-EXPOSE 8000:8000
-ENV VERSION 3.8.4
+EXPOSE 8000
+ENV VERSION 3.8.8
 
 # Executing group, with fixed group id
 ENV EXECUTING_GROUP fiduswriter
-ENV EXECUTING_GROUP_ID 999
+ENV EXECUTING_GROUP_ID 1000
 
 # Executing user, with fixed user id
 ENV EXECUTING_USER fiduswriter
-ENV EXECUTING_USER_ID 999
+ENV EXECUTING_USER_ID 1000
 
-# Data volume, should be owned by 999:999 to ensure the application can
-# function correctly. Run `chown 999:999 <data-dir-path>` on the host OS to
+# Data volume, should be owned by 1000:1000 to ensure the application can
+# function correctly. Run `chown 1000:1000 <data-dir-path>` on the host OS to
 # get this right.
 VOLUME ["/data"]
 
@@ -29,7 +29,6 @@ RUN groupadd \
         --gid ${EXECUTING_USER} \
         ${EXECUTING_USER}
 
-
 # Chain apt-get update, apt-get install and the removal of the lists.
 # This is one of the best practices of Docker, see
 # https://docs.docker.com/engine/userguide/eng-image/dockerfile_best-practices/#apt-get
@@ -39,6 +38,7 @@ RUN apt-get update \
         gettext \
         git \
         libjpeg-dev \
+        libpq-dev \
         npm \
         python3-venv \
         python3-dev \
@@ -52,14 +52,19 @@ RUN apt-get update \
 RUN curl -sL https://deb.nodesource.com/setup_10.x | bash
 RUN apt-get update && apt-get install -y nodejs && rm -rf /var/lib/apt/lists/*
 
-RUN pip3 install --upgrade setuptools
-RUN pip3 install fiduswriter==${VERSION}
-RUN pip3 install --upgrade pip wheel
+RUN pip3 install --upgrade setuptools pip wheel
+RUN pip3 install psycopg2==2.8.4 dj-database-url
 
+RUN pip3 install fiduswriter[books,citation-api-import]==${VERSION}
+
+RUN mkdir -p /fiduswriter && \
+        chown 1000:1000 /fiduswriter && \
+        chmod -R 777 /fiduswriter /data
+
+USER 1000:1000
 # Working directories should be absolute.
 # https://docs.docker.com/engine/userguide/eng-image/dockerfile_best-practices/#workdir
 WORKDIR /fiduswriter
-RUN chmod -R 777 /fiduswriter /data
 
 RUN python3 -m venv venv
 RUN /bin/bash -c "source /fiduswriter/venv/bin/activate"
--- start-fiduswriter.sh
+++ start-fiduswriter.sh
@@ -1,8 +1,12 @@
-#!/bin/sh -e
+#!/usr/bin/env sh -e
 #
 # Script to run fiduswriter and/or perform the initial setup 
 
 echo "*** Running as user $(whoami) ..."
+
+echo "*** Normalising UID and GID of /data volume ..."
+chown -R 1000:1000 /data
+chmod -R 0777 /data
 
 if [ -f /data/configuration.py ]; then
     echo '*** Fiduswriter will use the settings found in /data/configuration.py ...'
@@ -11,14 +15,18 @@ else
     fiduswriter startproject
 fi
 
-# If setup-performed.info does not yet exist, we assume that this is a "fresh" container and perform the setup.
-# This should also take care of any transpiling that may be required between versions.
-# This is however not an ideal solution as it makes the container stateful meaning that it may perform
-# different operations even if the same input data is used (i.e. the files from the /data directory are the same)
-if ! [ -f /fiduswriter/setup-performed.info ]; then
-    echo '*** Executing fiduswriter setup as this is the first time that this container is launched ...'
-	fiduswriter setup
-	touch /fiduswriter/setup-performed.info
+# Put the supplied configuration.py in place before running setup.
+if [ -f /fiduswriter/configuration.py ] ; then
+  if ! [ -L /fiduswriter/configuration.py ] ; then
+	if [ -f /data/configuration.py ] ; then
+		rm /fiduswriter/configuration.py
+	else
+	    mv /fiduswriter/configuration.py /data/configuration.py
+	fi
+  fi
+fi
+if ! [ -L /fiduswriter/configuration.py ] ; then
+  ln -sf /data/configuration.py /fiduswriter/configuration.py
 fi
 
 # If the fiduswriter/media directory exists, this means that a symlink can not directly
@@ -38,30 +46,14 @@ if ! [ -L /fiduswriter/media ] ; then
   ln -sf /data/media /fiduswriter
 fi
 
-if [ -f /fiduswriter/configuration.py ] ; then
-  if ! [ -L /fiduswriter/configuration.py ] ; then
-	if [ -f /data/configuration.py ] ; then
-		rm /fiduswriter/configuration.py
-	else
-	    mv /fiduswriter/configuration.py /data/configuration.py
-	fi
-  fi
-fi
-if ! [ -L /fiduswriter/configuration.py ] ; then
-  ln -sf /data/configuration.py /fiduswriter/configuration.py
-fi
-
-if [ -f /fiduswriter/fiduswriter.sql ] ; then
-  if ! [ -L /fiduswriter/fiduswriter.sql ] ; then
-	if [ -f /data/fiduswriter.sql ] ; then
-		rm /fiduswriter/fiduswriter.sql
-	else
-	    mv /fiduswriter/fiduswriter.sql /data/fiduswriter.sql
-	fi
-  fi
-fi
-if ! [ -L /fiduswriter/fiduswriter.sql ] ; then
-  ln -sf /data/fiduswriter.sql /fiduswriter/fiduswriter.sql
+# If setup-performed.info does not yet exist, we assume that this is a "fresh" container and perform the setup.
+# This should also take care of any transpiling that may be required between versions.
+# This is however not an ideal solution as it makes the container stateful meaning that it may perform
+# different operations even if the same input data is used (i.e. the files from the /data directory are the same)
+if ! [ -f /fiduswriter/setup-performed.info ]; then
+    echo '*** Executing fiduswriter setup as this is the first time that this container is launched ...'
+	fiduswriter setup
+	touch /fiduswriter/setup-performed.info
 fi
 
 # Run the fiduswriter-server
