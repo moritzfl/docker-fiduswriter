807e67b892ca5cc901366dd4b98ffc0e93c4ab01
ecobytes collective <status@ecobytes.net>
2020-03-31 03:28:33 +0200

    update examples with actual production environment
    - docker-compose.yaml integrates with a reverse proxy
    - the environment now knows how to close its Emaul with STARTTLS
    - the startup script avoids a race condition with trying to move media before its collected

--- start-fiduswriter.sh
+++ start-fiduswriter.sh
@@ -29,6 +29,16 @@ if ! [ -L /fiduswriter/configuration.py ] ; then
   ln -sf /data/configuration.py /fiduswriter/configuration.py
 fi
 
+# If setup-performed.info does not yet exist, we assume that this is a "fresh" container and perform the setup.
+# This should also take care of any transpiling that may be required between versions.
+# This is however not an ideal solution as it makes the container stateful meaning that it may perform
+# different operations even if the same input data is used (i.e. the files from the /data directory are the same)
+if ! [ -f /fiduswriter/setup-performed.info ]; then
+    echo '*** Executing fiduswriter setup as this is the first time that this container is launched ...'
+	fiduswriter setup
+	touch /fiduswriter/setup-performed.info
+fi
+
 # If the fiduswriter/media directory exists, this means that a symlink can not directly
 # be created. Instead we need to do the following before creating the symlink:
 # - remove the fiduswriter/media directory, if the user already has data/fiduswriter
@@ -44,16 +54,6 @@ if [ -d /fiduswriter/media ] ; then
 fi
 if ! [ -L /fiduswriter/media ] ; then
   ln -sf /data/media /fiduswriter
-fi
-
-# If setup-performed.info does not yet exist, we assume that this is a "fresh" container and perform the setup.
-# This should also take care of any transpiling that may be required between versions.
-# This is however not an ideal solution as it makes the container stateful meaning that it may perform
-# different operations even if the same input data is used (i.e. the files from the /data directory are the same)
-if ! [ -f /fiduswriter/setup-performed.info ]; then
-    echo '*** Executing fiduswriter setup as this is the first time that this container is launched ...'
-	fiduswriter setup
-	touch /fiduswriter/setup-performed.info
 fi
 
 # Run the fiduswriter-server
